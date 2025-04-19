8309e361516fdf62b776b3c782013db71ab9c24a
Vijay <vvreddy2424@gmail.com>
2025-04-19 22:58:51 +0530

    Initial commit - added HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Git World!");
-        System.out.println("Currently learning Git");
+        System.out.println("Hello, World!");
     }
 }
